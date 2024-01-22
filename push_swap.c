b7dfb71bbcfbf7ee35ad34ac48e0fc439e418630
unknown <7353718@gmail.com>
2024-01-22 20:55:02 +0100

    add ft_reversrotate

--- push_swap.c
+++ push_swap.c
@@ -44,6 +44,71 @@ void ft_print_Stack(t_list * st)
 	//printf("test st->n = %d\n", st->n);
 }
 
+int reverseRotate(t_list **list)
+{
+    t_list *current;
+    t_list *second_last;
+    t_list *last;
+
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return (-1); 
+
+    second_last = NULL;
+    current = *list;
+
+    while (current->next != NULL)
+    {
+        second_last = current;
+        current = current->next;
+    }
+    if (second_last == NULL)
+        return (0);
+
+    last = current;
+    second_last->next = NULL;
+    last->next = *list;
+    *list = last;
+    return (0);
+}
+
+int rra(t_list **a_list)
+{
+	int result;
+	result = reverseRotate(a_list);
+	if(result == -1)
+		return(-1);
+	ft_putstr_fd("rra\n", 1);
+	return (0);
+}
+
+int rrb(t_list **b_list)
+{
+	int result;
+	result = reverseRotate(b_list);
+	if(result == -1)
+		return(-1);
+	ft_putstr_fd("rrb\n", 1);
+	return (0);
+}
+
+int rrr(t_list **a_list, t_list **b_list)
+{
+	int result;
+	if ((a_list == NULL || *a_list == NULL || (*a_list)->next == NULL) ||
+        (b_list == NULL || *b_list == NULL || (*b_list)->next == NULL)) 
+	{
+        return -1;
+    }
+	result = reverseRotate(a_list);
+	if(result == -1)
+		return(-1);
+	result = reverseRotate(b_list);
+	if(result == -1)
+		return(-1);
+	ft_putstr_fd("rrr\n", 1);
+	return (0);
+}
+
 int rotate(t_list **list) 
 {
     if (list == NULL || *list == NULL || (*list)->next == NULL) 
@@ -67,6 +132,7 @@ int ra(t_list **a_list)
 	ft_putstr_fd("ra\n", 1);
 	return (0);
 }
+
 int rb(t_list **b_list)
 {
 	int result;
@@ -76,6 +142,7 @@ int rb(t_list **b_list)
 	ft_putstr_fd("rb\n", 1);
 	return (0);
 }
+
 int rr(t_list **a_list, t_list **b_list)
 {
 	if ((a_list == NULL || *a_list == NULL || (*a_list)->next == NULL) ||
@@ -93,7 +160,6 @@ int rr(t_list **a_list, t_list **b_list)
 	ft_putstr_fd("rr\n", 1);
 	return (0);
 }
-
 
 int push(t_list **a_list, t_list **b_list) 
 {
@@ -249,9 +315,13 @@ void ft_push_swap(t_list **a, t_list **b)
 	rotate(b);
 	printf("_______________\n");
 	ft_print_Stack(*b);
-	rotate(a);
-	rotate(a);
-	rotate(a);
+	// rerotate(a);
+	// rotate(a);
+	// rotate(a);
+	printf("_______________\n");
+	ft_print_Stack(*a);
+	reverseRotate(a);
+	reverseRotate(a);
 	printf("_______________\n");
 	ft_print_Stack(*a);
 	//else
@@ -411,7 +481,6 @@ void	ft_lstclear(t_list **lst)
 		*lst = tmp;
 	}
 }
-
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
